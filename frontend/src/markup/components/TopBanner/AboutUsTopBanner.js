1ec73ef2ddfddcdb973ddb0908dde3a5d4f1174d
Amanuel Sahle <amanuelsahile2007@gmail.com>
2026-07-24 11:27:37 +0300

    about us href correction

--- frontend/src/markup/components/TopBanner/AboutUsTopBanner.js
+++ frontend/src/markup/components/TopBanner/AboutUsTopBanner.js
@@ -1,19 +1,26 @@
-import React from 'react'
-import aboutUsBanner from "../../../assets/images/banner/AboutUsBanner.png"
+import React from "react";
+import aboutUsBanner from "../../../assets/images/banner/AboutUsBanner.png";
 
 function AboutUsTopBanner() {
-    return (
-        <div><section className="page-title" style={{ backgroundImage: `url(${aboutUsBanner})` }}>
-            <div className="auto-container">
-                <h2>About us</h2>
-                <ul className="page-breadcrumb">
-                    <li><a href="index.html">home</a></li>
-                    <li>About us</li>
-                </ul>
-            </div>
-            <h1 data-parallax='{"x": 200}'>About Us</h1>
-        </section></div>
-    )
+  return (
+    <div>
+      <section
+        className="page-title"
+        style={{ backgroundImage: `url(${aboutUsBanner})` }}
+      >
+        <div className="auto-container">
+          <h2>About us</h2>
+          <ul className="page-breadcrumb">
+            <li>
+              <a href="/about">home</a>
+            </li>
+            <li>About us</li>
+          </ul>
+        </div>
+        <h1 data-parallax='{"x": 200}'>About Us</h1>
+      </section>
+    </div>
+  );
 }
 
-export default AboutUsTopBanner
+export default AboutUsTopBanner;
